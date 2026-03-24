f9fbc5b1bccc1062234bfdf0cf44f7b74337237a
Edvinas <edvinasbureika@gmail.com>
2026-03-24 01:10:23 -0400

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#
+# SYSTEM
+#
+
 # Update the system
 sudo dnf update -y
 flatpak update -y
@@ -16,14 +20,23 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 # Install all the codecs
 sudo dnf install gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly gstreamer1-libav ffmpeg ffmpeg-libs ffmpeg-free lame libdvdcss -y
 
+#
+# APPS
+#
+
 # Install my must have software
 sudo dnf install vlc qbittorrent fastfetch htop vim neovim ranger git -y
 
-# Import my nvim config from my github page
-git clone https://github.com/0xEdvinas/.nvim.git ~/.config/nvim
+#
+# DEV
+#
 
 # Dev tools
 sudo dnf install python3 python3-pip -y
+
+#
+# VIRTUALIZATION
+#
 
 # QEMU and virt-manager for virtual machines
 sudo dnf install qemu-kvm libvirt virt-manager virt-viewer spice-vdagent -y
@@ -36,7 +49,9 @@ sudo systemctl start libvirtd
 # Add user to libvirt and kvm groups
 sudo usermod -aG libvirt,kvm $USER
 
-# Make filesystem paths i use
+#
+# FILESYSTEM
+#
 cd ~
 
 mkdir -p Personal/IDs
@@ -58,18 +73,26 @@ mkdir -p ISO
 mkdir -p Books/Audio
 mkdir -p Books/Text
 
+mkdir -p tmp/
+
+#
+# CONFIG LOADING
+#
+
+# Import my nvim config from my github page
+rm -rf ~/.config/nvim
+git clone https://github.com/0xEdvinas/.nvim.git ~/.config/nvim
+
 # Install hyperland
 echo "Do you want to install HyperLand and its config? (y/n)"
 read -r install_hyperland
-
 if [[ "$install_hyperland" == "y" || "$install_hyperland" == "Y" ]]; then
     echo "Installing HyperLand..."
 fi
 
 # Install QEMU Hooks
-echo "Do you want to install QEMU hooks? (y/n)"
+echo "Do you want to install Single Gpu Passthrough QEMU hooks? (y/n)"
 read -r qemu_hooks
-
-if [[ "qemu_hooks" == "y" || "qemu_hooks" == "Y" ]]; then
+if [[ "$qemu_hooks" == "y" || "$qemu_hooks" == "Y" ]]; then
     echo "Installing QEMU Hooks"
 fi
